732614ce8d323c3fa7c9b8698b86ba07c4d19aaf
Đào Hoa Nữ <hoanu14302@gmail.com>
2023-11-17 22:54:17 +0700

    kiểm tra người dùng

---
+++ admin/ktnguoidung/index.php
@@ -0,0 +1,42 @@
+<?php
+require("../../model/database.php");
+require("../../model/nguoidung.php");
+
+// Biến $isLogin cho biết người dùng đăng nhập chưa
+$isLogin = isset($_SESSION["nguoidung"]);
+// Kiểm tra hành động $action: yêu cầu đăng nhập nếu chưa xác thực
+if (isset($_REQUEST["action"])) {
+    $action = $_REQUEST["action"];
+} elseif ($isLogin == FALSE) {
+    $action = "dangnhap";
+} else {
+    $action = "macdinh";
+}
+$nd = new NGUOIDUNG();
+switch ($action) {
+    case "macdinh":
+        include("main.php");
+        break;
+    case "dangnhap":
+        include("login.php");
+        break;
+    case "xldangnhap":
+        $email = $_POST["txtemail"];
+        $matkhau = $_POST["txtmatkhau"];
+        if ($nd->kiemtranguoidunghople($email, $matkhau) == TRUE) {
+            $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
+            include("main.php");
+        } else {
+            include("login.php");
+        }
+        break;
+    case "dangxuat":
+        unset($_SESSION["nguoidung"]);
+        include("login.php");
+        break;
+    case "hoso":
+            include("profile.php");
+            break;
+    default:
+        break;
+}
