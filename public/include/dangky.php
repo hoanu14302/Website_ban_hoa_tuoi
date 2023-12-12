3f6df300132819a4c5a6d943c5ef9311d0c41905
Lương Gia Thịnh <gthinh.lgt1302@gmail.com>
2023-12-12 17:30:34 +0700

    update dangky

--- public/include/dangky.php
+++ public/include/dangky.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, 
 shrink-to-fit=no">
     <link rel="preconnect" href="https://">
-    <title>Đăng ký - ABC Shop</title>
+    <title>Đăng ký - Shop bán hoa tươi</title>
     <link href="../inc/css/app.css" rel="stylesheet">
     <script src="../inc/js/app.js"></script>
     <link href="https://" rel="stylesheet">
