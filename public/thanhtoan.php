60a2f7b4ab237e933e88071f773592907afd1788
Lương Gia Thịnh <gthinh.lgt1302@gmail.com>
2023-12-14 22:11:30 +0700

    fix

--- public/thanhtoan.php
+++ public/thanhtoan.php
@@ -49,7 +49,7 @@
                     <?php foreach ($giohang as $id => $mh): ?>
                         <input type="hidden" name="txtsl" value="<?php echo $mh['soluong']; ?>">
                         <tr>
-                            <td><img width="50" src="../<?php echo $mh["hinhanh"]; ?>" alt=""> <?php echo $mh["tenmh"]; ?></td>
+                            <td><img width="50" src="../img/giohang/<?php echo $mh["hinhanh"]; ?>" alt=""> <?php echo $mh["tenmh"]; ?></td>
                             <td><?php echo number_format($mh["giaban"]); ?>đ</td>
                             <td><?php echo $mh['soluong']; ?></td>
                             <td><?php echo number_format($mh["thanhtien"]); ?>đ</td>
