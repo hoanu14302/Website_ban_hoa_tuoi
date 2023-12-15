130cbad46c1f67a6788f94d8ad368a32d30be608
Đào Hoa Nữ <hoanu14302@gmail.com>
2023-12-15 21:09:12 +0700

    Update timkiem.php

--- public/timkiem.php
+++ public/timkiem.php
@@ -7,43 +7,45 @@
         echo "<p>Kết quả tìm kiếm không có ! Vui lòng nhập từ khóa khác...</p>";
     } else {
         foreach ($mathang as $m) : {
-            $tendm = $dm->laydanhmuctheoid($m["danhmuc_id"]);
+                foreach ($danhmuc as $dm) :
+                    if ($m["danhmuc_id"] == $dm["id"]) {
     ?>
-                <div class="col mb-5">
-                    <div class="card h-100 shadow">
+                        <div class="col mb-5">
+                            <div class="card h-100 shadow">
 
-                        <!-- Sale badge-->
-                        <?php if ($m["giaban"] < $m["giagoc"]) { ?>
-                            <div class="badge bg-danger text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Giảm giá</div>
-                        <?php } // end if 
-                        ?>
-                        <!-- Product image-->
-                        <a href="index.php?action=detail&danhmuc_id=<?php echo $m["danhmuc_id"]; ?>&id=<?php echo $m["id"]; ?>">
-                            <img class="card-img-top" src="../img/hoa/<?php echo $tendm; ?>/<?php echo $m["hinhanh"]; ?>" alt="<?php echo $m["tenmh"]; ?>" style="height: 300px;" />
-                        </a>
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <a class="text-decoration-none" href="index.php?action=detail&danhmuc_id=<?php echo $m["danhmuc_id"]; ?>&id=<?php echo $m["id"]; ?>">
-                                    <h5 class="fw-bolder text-info"><?php echo $m["tenmh"]; ?></h5>
-                                </a>
-                                <!-- Product price-->
+                                <!-- Sale badge-->
                                 <?php if ($m["giaban"] < $m["giagoc"]) { ?>
-                                    <span class="text-muted text-decoration-line-through"><?php echo number_format($m["giagoc"]); ?>đ</span><?php } // end if 
-                                                                                                                                            ?>
-                                <span class="text-danger fw-bolder"><?php echo number_format($m["giaban"]); ?>đ</span>
+                                    <div class="badge bg-danger text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Giảm giá</div>
+                                <?php } // end if 
+                                ?>
+                                <!-- Product image-->
+                                <a href="index.php?action=detail&danhmuc_id=<?php echo $m["danhmuc_id"]; ?>&id=<?php echo $m["id"]; ?>">
+                                    <img class="card-img-top" src="../img/hoa/<?php echo $dm["tendm"]; ?>/<?php echo $m["hinhanh"]; ?>" alt="<?php echo $m["tenmh"]; ?>" style="height: 300px;" />
+                                </a>
+                                <!-- Product details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- Product name-->
+                                        <a class="text-decoration-none" href="index.php?action=detail&danhmuc_id=<?php echo $m["danhmuc_id"]; ?>&id=<?php echo $m["id"]; ?>">
+                                            <h5 class="fw-bolder text-info"><?php echo $m["tenmh"]; ?></h5>
+                                        </a>
+                                        <!-- Product price-->
+                                        <?php if ($m["giaban"] < $m["giagoc"]) { ?>
+                                            <span class="text-muted text-decoration-line-through"><?php echo number_format($m["giagoc"]); ?>đ</span><?php } // end if 
+                                                                                                                                                    ?>
+                                        <span class="text-danger fw-bolder"><?php echo number_format($m["giaban"]); ?>đ</span>
+                                    </div>
+                                </div>
+                                <!-- Product actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-info mt-auto" href="index.php?action=chovaogio&danhmuc_id=<?php echo $m["danhmuc_id"]; ?>&id=<?php echo $m["id"]; ?>&soluong=1">Chọn mua</a></div>
+                                </div>
                             </div>
                         </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-info mt-auto" href="index.php?action=chovaogio&danhmuc_id=<?php echo $m["danhmuc_id"]; ?>&id=<?php echo $m["id"]; ?>&soluong=1">Chọn mua</a></div>
-                        </div>
-                    </div>
-                </div>
-    <?php
+    <?php }
+                endforeach;
             }
-            endforeach;
+        endforeach;
     }
     ?>
 </div>
