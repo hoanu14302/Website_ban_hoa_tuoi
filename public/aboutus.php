283b51568bc2384326b7f47c7f392920eb4454a6
NhanPham2710 <odqthihi@gmail.com>
2023-12-15 20:36:31 +0700

    fix aboutus

--- public/aboutus.php
+++ public/aboutus.php
@@ -5,15 +5,15 @@
 		<div class="large-12 col">
 			<div class="col-inner">							
 				<div class="container section-title-container"><h2 class="section-title section-title-center"><b></b><span class="section-title-main">Về Chúng Tôi</span><b></b></h2></div>
-                    <p style="text-align: justify;">Hoa là món quà vô giá mà thiên nhiên đã dành tặng cho con người. Mỗi loài hoa mang một vẻ đẹp và sắc thái riêng, chính vì vậy mà người thưởng thức hoa có thể gởi gắm tâm tư tình cảm của mình vào giỏ hoa một cách tinh tế, ý nghĩa</p>
+                    <p style="text-align: justify;">Hoa là món quà vô giá mà thiên nhiên đã dành tặng cho con người. Mỗi loài hoa mang một vẻ đẹp và sắc thái riêng, chính vì vậy mà người thưởng thức hoa có thể gởi gắm tâm tư tình cảm của mình vào giỏ hoa một cách tinh tế, đầy ý nghĩa.</p>
                     <p style="text-align: justify;">Bên cạnh đó, mỗi loài hoa còn mang một ý nghĩa đặc biệt, hiểu được nó sẽ giúp bạn tránh khỏi những bối rối không nói nên lời trước một tình yêu e ấp, trước một tình bạn bền lâu, một lòng biết ơn vô bờ bến đối với cha với mẹ, hay chỉ đơn giản là một lời cảm ơn sâu sắc, chân thành…</p>
                     <p style="text-align: justify;">Đến với Elko Garden các bạn sẽ được nhìn ngắm đầy đủ các loại hoa và kiểu hoa được kết khéo léo hoàn toàn mới nhưng vẫn toát lên vẻ đẹp tự nhiên vốn có của mỗi loài hoa. Từ những nguyên liệu cơ bản kết hợp với sự khéo léo của đôi bàn tay người thợ, Elko Garden đã thổi vào đó một hồn hoa tinh tế với màu sắc thật tự nhiên, mềm mại. Bên cạnh vẻ đẹp tươi tắn, sinh động đó, mỗi giỏ hoa còn có độ tươi bền lâu, góp phần tăng thêm giá trị vẻ đẹp.</p>
                     <p style="text-align: justify;">Với những ưu điểm nổi bật trên, Elko Garden mong muốn đem đến cho quý khách những sản phẩm hoa thân thiện, gần gũi, góp phần trang trí cho không gian sống của gia đình bạn thêm sinh động, mới mẻ. Bên cạnh đó, giá cả hợp lí phải chăng sẽ giúp quý khách có được sự lựa chọn tốt nhất cho sản phẩm mình yêu thích.</p>
                     <p style="text-align: justify;">Ngoài ra, chúng tôi chuyên nhận đặt hoa cho các chương trình sự kiện lớn như: hội thảo, cưới hỏi… vv…</p>
                     <p style="text-align: justify;">Đến với Elko Garden, là đến với những mẫu hoa độc đáo, luôn được trang trí ấn tượng, sang trọng, mang tính nghệ thuật truyền cảm hứng cao tới người được nhận.</p>
                     <p style="text-align: justify;">Chúng tôi luôn sẵn sàng phục vụ Quý khách, với sự tận tình chu đáo, bằng nhiều sản phẩm và dịch vụ đa dạng, chuyển hoa miễn phí trong các dịp lễ tết, một cách nhanh chóng nhất.</p>
-<p style="text-align: justify;">Thông điệp mà chúng tôi muốn gửi tới khách hàng là chúc Quý vị luôn vui vẻ, hạnh phúc và có cuộc sống thật tươi đẹp.</p>
-                    <p style="text-align: justify;">Chúng tôi xin chân thành cảm ơn sự ủng hộ của Quý Vị.</p>
+<p style="text-align: justify;">Thông điệp mà chúng tôi muốn gửi tới khách hàng là chúc Quý khách luôn vui vẻ, hạnh phúc và có cuộc sống thật tươi đẹp.</p>
+                    <p style="text-align: justify;">Chúng tôi xin chân thành cảm ơn sự ủng hộ của Quý khách.</p>
                     <img src="../images/nen_gioithieu.jpg" style="width: 100%;" alt="">
                 </div>
             </div>
