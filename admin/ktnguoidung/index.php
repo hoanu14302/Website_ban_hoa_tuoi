5d3028ecb4443234bcbced5b7821eb47b63a8b0f
Đào Hoa Nữ <hoanu14302@gmail.com>
2023-12-15 13:59:00 +0700

    Update index.php

--- admin/ktnguoidung/index.php
+++ admin/ktnguoidung/index.php
@@ -1,7 +1,7 @@
 <?php
-require("../model/database.php");
-require("../model/nguoidung.php");
-
+require("../../model/database.php");
+require("../../model/nguoidung.php");
+require("../../model/quyen.php");
 // Biến $isLogin cho biết người dùng đăng nhập chưa
 $isLogin = isset($_SESSION["nguoidung"]);
 // Kiểm tra hành động $action: yêu cầu đăng nhập nếu chưa xác thực
@@ -13,9 +13,10 @@ if (isset($_REQUEST["action"])) {
     $action = "macdinh";
 }
 $nd = new NGUOIDUNG();
+$pq = new QUYEN();
 switch ($action) {
     case "macdinh":
-        include("main.php");
+        include("profile.php");
         break;
     case "dangnhap":
         include("login.php");
@@ -26,6 +27,8 @@ switch ($action) {
         if ($nd->kiemtranguoidunghople($email, $matkhau) == TRUE) {
             $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
             include("main.php");
+        // } elseif( $_SESSION["nguoidung"]["loai"] == 3 ){
+        //     include("../../../public/main.php");
         } else {
             include("login.php");
         }
@@ -35,8 +38,51 @@ switch ($action) {
         include("login.php");
         break;
     case "hoso":
-            include("profile.php");
-            break;
+        include("profile.php");
+        break;
+    case "xlhoso":
+        $mand = $_POST["txtid"];
+        $email = $_POST["txtemail"];
+        $sodt = $_POST["txtsdt"];
+        $diachi = $_POST["txtdiachi"];
+        $hoten = $_POST["txthoten"];
+        $hinhanh = $_POST["txthinhanh"];
+
+        if($_FILES["fhinhanh"]["name"] != null) {
+            $hinhanh = basename($_FILES["fhinhanh"]["name"]);
+            $duongdan = "../../images/users/" . $hinhanh;
+            move_uploaded_file($_FILES["fhinhanh"]["tmp_name"], $duongdan);
+        }
+        $nd->capnhatnguoidung($mand,$email,$sodt,$hoten,$hinhanh,$diachi);
+        $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
+        include("main.php");
+        break;
+    case "dangky":
+        include("register.php");
+        break;
+    case "xldangky":
+        $loai = $_POST["txtloai"];
+        //xử lý load ảnh
+        $hinhanh = basename($_FILES["fhinhanh"]["name"]); // đường dẫn ảnh lưu trong db
+        $duongdan = "../../images/products/" . $hinhanh; //nơi lưu file upload
+        move_uploaded_file($_FILES["fhinhanh"]["tmp_name"], $duongdan);
+        //xử lý thêm mặt hàng
+        $nguoidungmoi = new NGUOIDUNG();
+        $nguoidungmoi->setemail($_POST["txtemail"]);
+        $nguoidungmoi->setsodienthoai($_POST["txtsodienthoai"]);
+        $nguoidungmoi->setdiachi($_POST["txtdiachi"]);
+        $nguoidungmoi->setmatkhau($_POST["txtmatkhau"]);
+        $nguoidungmoi->sethoten($_POST["txthoten"]);
+        $nguoidungmoi->setloai($loai);
+        $nguoidungmoi->settrangthai($_POST["txttrangthai"]);
+        $nguoidungmoi->sethinhanh($hinhanh);
+        // thêm
+        $nd->themnguoidung($nguoidungmoi);
+        // load người dùng
+        $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($_POST["txtemail"]);
+        include("profile.php");
+        break;
     default:
         break;
 }
+?>
