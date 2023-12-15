563e39435eb02ff319d9898775d5133c3f971d39
NhanPham2710 <odqthihi@gmail.com>
2023-12-15 20:37:38 +0700

    thêm

--- public/aboutus.php
+++ public/aboutus.php
@@ -4,7 +4,7 @@
 	<div class="row row-main">
 		<div class="large-12 col">
 			<div class="col-inner">							
-				<div class="container section-title-container"><h2 class="section-title section-title-center"><b></b><span class="section-title-main">Về Chúng Tôi</span><b></b></h2></div>
+				<div class="container section-title-container"><h2 class="section-title section-title-center"><b></b><span class="section-title-main">Về Chúng Tôi - Elko Garden</span><b></b></h2></div>
                     <p style="text-align: justify;">Hoa là món quà vô giá mà thiên nhiên đã dành tặng cho con người. Mỗi loài hoa mang một vẻ đẹp và sắc thái riêng, chính vì vậy mà người thưởng thức hoa có thể gởi gắm tâm tư tình cảm của mình vào giỏ hoa một cách tinh tế, đầy ý nghĩa.</p>
                     <p style="text-align: justify;">Bên cạnh đó, mỗi loài hoa còn mang một ý nghĩa đặc biệt, hiểu được nó sẽ giúp bạn tránh khỏi những bối rối không nói nên lời trước một tình yêu e ấp, trước một tình bạn bền lâu, một lòng biết ơn vô bờ bến đối với cha với mẹ, hay chỉ đơn giản là một lời cảm ơn sâu sắc, chân thành…</p>
                     <p style="text-align: justify;">Đến với Elko Garden các bạn sẽ được nhìn ngắm đầy đủ các loại hoa và kiểu hoa được kết khéo léo hoàn toàn mới nhưng vẫn toát lên vẻ đẹp tự nhiên vốn có của mỗi loài hoa. Từ những nguyên liệu cơ bản kết hợp với sự khéo léo của đôi bàn tay người thợ, Elko Garden đã thổi vào đó một hồn hoa tinh tế với màu sắc thật tự nhiên, mềm mại. Bên cạnh vẻ đẹp tươi tắn, sinh động đó, mỗi giỏ hoa còn có độ tươi bền lâu, góp phần tăng thêm giá trị vẻ đẹp.</p>
