dbb109d2d22226fb14e5ecb15352e4af60470dc8
NhanPham2710 <odqthihi@gmail.com>
2023-12-16 21:02:17 +0700

    fix session

--- admin/ktnguoidung/index.php
+++ admin/ktnguoidung/index.php
@@ -31,7 +31,8 @@ switch ($action) {
         $email = $_POST["txtemail"];
         $matkhau = $_POST["txtmatkhau"];
         if ($nd->kiemtranguoidunghople($email, $matkhau) == TRUE) {
-            if ($_SESSION["nguoidung"]["loai"] == 1 || $_SESSION["nguoidung"]["loai"] == 2) {
+            if (isset($_SESSION["nguoidung"]) && $_SESSION["nguoidung"] !== null && isset($_SESSION["nguoidung"]["loai"])) {
+
                 $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
                 $mathanghh = $mh->laymathanghethang();
                 include("main.php");
